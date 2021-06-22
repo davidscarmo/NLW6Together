0d10557a782f0716305711be80fb70a8f6ab954f
David Carmo <davidsc2812@gmail.com>
2021-06-21 23:19:00 -0300

    Criacao das rotas e navegacao entre a pagina home e a newroom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-
+import { BrowserRouter ,Route} from 'react-router-dom';
 import './styles/global.scss';
 function App() {
   return (
-  <NewRoom />
+    <BrowserRouter>
+    <Route path="/" exact component={Home} />
+    <Route path="/rooms/new" component={NewRoom}/>
+    </BrowserRouter>
+  
   );
 }
 
