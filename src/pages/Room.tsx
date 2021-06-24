1549618358b7ef54bc7a9228b30fd40a0e521113
David Carmo <davidsc2812@gmail.com>
2021-06-23 21:43:17 -0300

    Finalizacao da estrutura e estilizacao das perguntas

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import logoImg from "../assets/images/logo.svg";
 import { Button } from "../components/Button";
+import { Question } from "../components/Question";
 import { RoomCode } from "../components/RoomCode";
 import { useAuth } from "../hooks/useAuth";
 import { database } from "../services/firebase";
@@ -21,7 +22,7 @@ type FirebaseQuestions = Record<string, {
   isHighlighted: boolean;
 }>;
 
-type Question = {
+type QuestionType = {
   id: string;
   author: 
   {
@@ -39,7 +40,7 @@ export const Room = () => {
   const { user } = useAuth();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState("");
-  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
   const roomId = params.id;
 
@@ -128,7 +129,17 @@ export const Room = () => {
               Enviar pergunta
             </Button>
           </div>
-          {JSON.stringify(questions)}
+          <div className="question-list">
+          {questions.map((question) => {
+            return (
+              <Question
+              key={question.id}
+              content={question.content}
+              author={question.author}
+              />
+            )
+          })}
+          </div>
         </form>
       </main>
     </div>
