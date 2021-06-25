9321997f5713536ad4cfef3429be6d02c032a06c
David Carmo <davidsc2812@gmail.com>
2021-06-25 18:06:22 -0300

    Adicao do modal para confirmacao de exclusao da perguntar por parte do admin

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -4,6 +4,7 @@ import deleteImage from "../assets/images/delete.svg";
 import checkImg from "../assets/images/check.svg";
 import answerImg from "../assets/images/answer.svg";
 
+import Modal from "react-modal";
 import { Button } from "../components/Button";
 import { Question } from "../components/Question";
 import { RoomCode } from "../components/RoomCode";
@@ -12,6 +13,7 @@ import { useRoom } from "../hooks/useRoom";
 
 import "../styles/room.scss";
 import { database } from "../services/firebase";
+import { useState } from "react";
 
 type RoomParams = {
   id: string;
@@ -22,7 +24,17 @@ export const AdminRoom = () => {
   const params = useParams<RoomParams>();
   const roomId = params.id;
   const { questions, title } = useRoom(roomId);
-
+  const [confirmationModal, setConfirmationModal] = useState(false);
+  const [questionIdToDelete, setQuestionIdToDelete] = useState("");
+  
+  const handleOpenConfirmationModal = (questionId: string) => {
+    setConfirmationModal(true);
+    setQuestionIdToDelete(questionId);
+  };
+  const handleCloseConfirmationModal = () => {
+    setConfirmationModal(false);
+  };
+  
   const handleEndRoom = async () => {
     await database.ref(`rooms/${roomId}`).update({
       endedAt: new Date(),
@@ -31,9 +43,8 @@ export const AdminRoom = () => {
     history.push("/");
   };
   const handleDeleteQuestion = async (questionId: string) => {
-    if (window.confirm("Tem certeza que deseja excluir essa pergunta?")) {
-      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
-    }
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    setConfirmationModal(false);
   };
 
   const handleCheckQuestionAsAnswered = async (questionId: string) => {
@@ -100,7 +111,7 @@ export const AdminRoom = () => {
                 )}
                 <button
                   type="button"
-                  onClick={() => handleDeleteQuestion(question.id)}
+                  onClick={() => handleOpenConfirmationModal(question.id)}
                 >
                   <img src={deleteImage} alt="Remover pergunta" />
                 </button>
@@ -109,6 +120,40 @@ export const AdminRoom = () => {
           })}
         </div>
       </main>
+
+      <Modal
+        isOpen={confirmationModal}
+        onRequestClose={handleCloseConfirmationModal}
+        className="Modal"
+        overlayClassName="Overlay"
+      >
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M3 5.99988H5H21"
+            stroke="#737380"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M8 5.99988V3.99988C8 3.46944 8.21071 2.96074 8.58579 2.58566C8.96086 2.21059 9.46957 1.99988 10 1.99988H14C14.5304 1.99988 15.0391 2.21059 15.4142 2.58566C15.7893 2.96074 16 3.46944 16 3.99988V5.99988M19 5.99988V19.9999C19 20.5303 18.7893 21.039 18.4142 21.4141C18.0391 21.7892 17.5304 21.9999 17 21.9999H7C6.46957 21.9999 5.96086 21.7892 5.58579 21.4141C5.21071 21.039 5 20.5303 5 19.9999V5.99988H19Z"
+            stroke="#737380"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+
+        <h2>Excluir Pergunta</h2>
+        <p>Tem certeza que você deseja excluir esta pergunta?</p>
+
+        <div className="confirmArea">
+          <button type="button" onClick={handleCloseConfirmationModal}>
+            Cancelar
+          </button>
+          <button type="button" onClick={() => handleDeleteQuestion(questionIdToDelete)}>Sim, Excluir</button>
+        </div>
+      </Modal>
     </div>
   );
 };
